4d2455297e0861c94c76b35b8c8a429e7f1e0152
cryoz <hellion.zz@gmail.com>
2025-05-23 15:08:40 +0300

    add serial bootloader reboot for gd32 fws

--- src/gd32/gd32f30x.c
+++ src/gd32/gd32f30x.c
@@ -211,8 +211,16 @@ systemInit(void)
 
 
 void
+bootloader_request(void)
+{
+    dfu_reboot();
+}
+
+
+void
 armcm_main(void)
 {
+	dfu_reboot_check();
 	systemInit();
 	
 	SCB->VTOR = (uint32_t)VectorTable;
